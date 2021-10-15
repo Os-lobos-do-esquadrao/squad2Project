35dfc0a2a254626dc2c9cd317b6ef379b22a4b59
Davi Teixeira <davi@furg.br>
2021-10-15 11:23:53 -0300

    Create SecondTab

--- src/components/SecondTab/index.js
+++ src/components/SecondTab/index.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BoxInput, LabelText, InputText, Button } from '../base/BaseElements';
 
+const SecondTab = () => {
+  return (
+    <form action="#">
+      <BoxInput>
+        <LabelText for="Linkedin">Linkedin</LabelText>
+        <InputText
+          id="Linkedin"
+          placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/"
+        />
+      </BoxInput>{' '}
+      <BoxInput>
+        <LabelText for="Github">Github *</LabelText>
+        <InputText
+          id="Github"
+          placeholder="https://github.com/foobar"
+          required
+        />
+      </BoxInput>
+      <Button style={{}}>Next</Button>
+    </form>
+  );
+};
+
+export default SecondTab;
